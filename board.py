84f9822289b68d7a6362d615a3feec22d29e9da4
ha-seu <ha.seu@northeastern.edu>
2021-12-05 22:37:10 -0500

    Created all checks and move function

--- board.py
+++ board.py
@@ -35,5 +35,40 @@ class Board:
             if self.playboard[y][x+1].color != color and self.playboard[y][x+1].color !=0:
                 if self.playboard[y][x+2].color == color:
                     return True
-
-    #def do_move(self, coord, color):
+        # up check
+        if y > 2:
+            if self.playboard[y-1][x].color != color and self.playboard[y-1][x].color !=0:
+                if self.playboard[y-2][x].color == color:
+                    return True
+        # down check
+        if y < self.height-1:
+            if self.playboard[y+1][x].color != color and self.playboard[y+1][x].color !=0:
+                if self.playboard[y+2][x].color == color:
+                    return True
+        # up-left check
+        if x > 2 and y > 2:
+            if self.playboard[y-1][x-1].color != color and self.playboard[y-1][x-1].color != 0:
+                if self.playboard[y-1][x-2].color == color:
+                    return True
+        # up-right check
+        if x < self.width-1 and y > 2:
+            if self.playboard[y-1][x+1].color != color and self.playboard[y-1][x+1].color != 0:
+                if self.playboard[y-1][x+2].color == color:
+                    return True
+        # down-right check
+        if x < self.width-1 and y < self.height-1:
+            if self.playboard[y+1][x+1].color != color and self.playboard[y+1][x+1].color != 0:
+                if self.playboard[y+1][x+2].color == color:
+                    return True
+        # down-left check
+        if x > 2 and y < self.height-1:
+            if self.playboard[y+1][x-1].color != color and self.playboard[y+1][x-1].color != 0:
+                if self.playboard[y+1][x-2].color == color:
+                    return True
+    def do_move(self, coord, color):
+        x = int(coord[0]) - 1
+        y = int(coord[2]) - 1
+        if color == 1:
+            self.playboard[y][x].change_white()
+        if color == 2:
+            self.playboard[y][x].change_black()
--- main.py
+++ main.py
@@ -5,9 +5,18 @@ width = int(input("How wide would you like the board to be? Enter Width\n"))
 height = int(input("How tall would you like the board to be? Enter Height\n"))
 board = Board(width, height)
 print(board)
-#while(Piece.black > 0 and Piece.white > 0 and Piece.blank > 0):
-print("Black, it is your turn.")
-coord = input("Enter your coordinate selection in x,y format.\n")
-while(not board.valid_move(coord, 1)):
-    print("Invalid coordinate, please enter a valid placement")
+while(Piece.black > 0 and Piece.white > 0 and Piece.blank > 0):
+    print("Black, it is your turn.")
     coord = input("Enter your coordinate selection in x,y format.\n")
+    while(not board.valid_move(coord, 1)):
+        print("Invalid coordinate, please enter a valid placement")
+        coord = input("Enter your coordinate selection in x,y format.\n")
+    board.do_move(coord, 1)
+    print(board)
+    print("White, it is your turn.")
+    coord = input("Enter your coordinate selection in x,y format.\n")
+    while (not board.valid_move(coord, 2)):
+        print("Invalid coordinate, please enter a valid placement")
+        coord = input("Enter your coordinate selection in x,y format.\n")
+    board.do_move(coord, 2)
+    print(board)
