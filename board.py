b317a49cb65e663331809f59efb93cb7bcfd2cc5
ha-seu <ha.seu@northeastern.edu>
2021-12-05 23:40:51 -0500

    Started redoing check and move system

--- board.py
+++ board.py
@@ -5,10 +5,10 @@ class Board:
         self.width = width
         self.height = height
         self.playboard = [[Piece(0) for i in range(0, width)] for i in range(0, height)]
-        self.playboard[height // 2 - 1][width // 2].change_black()
-        self.playboard[height // 2][width // 2 - 1].change_black()
-        self.playboard[height // 2][width // 2].change_white()
-        self.playboard[height // 2 - 1][width // 2 - 1].change_white()
+        self.playboard[height // 2 - 1][width // 2].change_color(1)
+        self.playboard[height // 2][width // 2 - 1].change_color(1)
+        self.playboard[height // 2][width // 2].change_color(2)
+        self.playboard[height // 2 - 1][width // 2 - 1].change_color(2)
 
     def __str__(self):
         board = ""
@@ -26,19 +26,31 @@ class Board:
         x = int(coord[0])-1
         y = int(coord[2])-1
         list_of_valid = []
+        # right check
+        if x < self.width-2 and self.playboard[y][x+1] != color and self.playboard[y][x+1].color != 0:
+            for i in range(x+2,self.width-1):
+                if self.playboard[y][i].color == color:
+                    list_of_valid += [[y, i, 1]]
         # left check
         if x > 1 and self.playboard[y][x-1] != color and self.playboard[y][x-1].color != 0:
             for i in range(x-2,0,-1):
                 if self.playboard[y][i].color == color:
-                    list_of_valid += [y, i]
+                    list_of_valid += [[y, i, 5]]
+
         return list_of_valid
 
-    def do_move(self, coord, color, direction):
+    def do_move(self, coord, direction):
         x = int(coord[0]) - 1
         y = int(coord[2]) - 1
-        if color == 1:
-            self.playboard[direction[0]][direction[1]].change_black()
-            self.playboard[y][x].change_black()
-        if color == 2:
-            self.playboard[direction[0]][direction[1]].change_white()
-            self.playboard[y][x].change_white()
+        for i in direction:
+            # right flip
+            if i[2] == 1:
+                for k in range(x,i[1]):
+                    if self.playboard[y][k].color != self.playboard[i[0]][i[1]].color:
+                        self.playboard[y][k].flip()
+                    self.playboard[y][x].change_color(self.playboard[i[0]][i[1]].color)
+            if i[2] == 5:
+                for k in range(i[1],x):
+                    if self.playboard[y][k].color != self.playboard[i[0]][i[1]].color:
+                        self.playboard[y][k].flip()
+                    self.playboard[y][x].change_color(self.playboard[i[0]][i[1]].color)
--- piece.py
+++ piece.py
@@ -1,7 +1,9 @@
 class Piece:
+
     black = 0
     white = 0
     blank = 0
+
     # Initilization of the piece class taking the x and y coordinates as color data (0 = blank, 1 = black, 2 = white)
     def __init__(self, color):
         self.color = color
@@ -20,19 +22,31 @@ class Piece:
         if self.color == 0:
             return " "
 
-    def change_white(self):
-        if self.color == 1:
-            Piece.black -= 1
-        if self.color == 0:
-            Piece.blank -= 1
-        Piece.white += 1
-        self.color = 2
+    def change_color(self, color):
+        if color == 2:
+            if self.color == 1:
+                Piece.black -= 1
+            if self.color == 0:
+                Piece.blank -= 1
+            Piece.white += 1
+            self.color = 2
+        if color == 1:
+            if self.color == 2:
+                Piece.white -= 1
+            if self.color == 0:
+                Piece.blank -= 1
+            Piece.black += 1
+            self.color = 1
 
-    def change_black(self):
-        if self.color == 2:
+    def flip(self):
+        if self.color == 1:
+            Piece.white += 1
+            Piece.black -= 1
+            self.color = 2
+            print("flipping to white")
+        else:
             Piece.white -= 1
-        if self.color == 0:
-            Piece.blank -= 1
-        Piece.black += 1
-        self.color = 1
+            Piece.black += 1
+            self.color = 1
+            print("flipping to black")
 
